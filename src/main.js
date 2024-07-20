784817d71ac355111d530c609807b4fac55c1b3f
Salem Tarek <salemtarek224@gmail.com>
2024-07-20 15:13:54 +0300

    Fixing Images Links and install axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import 'tw-elements';
-
 import "./assets/scss/tailwind_config.scss";
+
+import axios from 'axios';
+window.axios = axios;
 
 createApp(App).use(store).use(router).mount("#app");
